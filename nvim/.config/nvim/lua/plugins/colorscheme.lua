34ac67f40402955b9d29118fffe5d1615d49833c
Wei Hu <wei7expadd@gmail.com>
2025-11-24 22:56:00 +0800

    update

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -43,13 +43,13 @@ return {
       which_key = true,
     },
   },
-  {
-    "akinsho/bufferline.nvim",
-    optional = false,
-    opts = function(_, opts)
-      if (vim.g.colors_name or ""):find("catppuccin") then
-        opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-      end
-    end,
-  },
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   optional = false,
+  --   opts = function(_, opts)
+  --     if (vim.g.colors_name or ""):find("catppuccin") then
+  --       opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+  --     end
+  --   end,
+  -- },
 }
