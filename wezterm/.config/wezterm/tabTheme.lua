18e029c4b4dc86b7c1c934ae23372c5d3cbb75ca
Wei Hu <wei7expadd@gmail.com>
2026-03-10 23:06:05 +0800

    feat: change to tabline style

--- wezterm/.config/wezterm/tabTheme.lua
+++ wezterm/.config/wezterm/tabTheme.lua
@@ -1,49 +1,50 @@
 local wezterm = require("wezterm")
-local tabTheme = {}
+--local tabTheme = {}
 --local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
-tabTheme.merge_tables = function(first_table, second_table, third_table)
-	for k, v in pairs(second_table) do
-		first_table[k] = v
-	end
-	for k, v in pairs(third_table) do
-		first_table[k] = v
-	end
-	return first_table
-end
+--tabTheme.merge_tables = function(first_table, second_table, third_table)
+--	for k, v in pairs(second_table) do
+--		first_table[k] = v
+--	end
+--	for k, v in pairs(third_table) do
+--		first_table[k] = v
+--	end
+--	return first_table
+--end
+--
+--tabTheme.extract_tab_bar_colors_from_theme = function(theme_name)
+--	local wez_theme = wezterm.color.get_builtin_schemes()[theme_name]
+--	return {
+--		window_frame_colors = {
+--			active_titlebar_bg = wez_theme.background,
+--			inactive_titlebar_bg = wezterm.color.parse(wez_theme.background):darken(0.8),
+--		},
+--		tab_bar_colors = {
+--			inactive_tab_edge = wezterm.color.parse(wez_theme.background):darken(0.8),
+--			active_tab = {
+--				bg_color = wez_theme.brights[3],
+--				fg_color = wez_theme.background,
+--			},
+--			inactive_tab = {
+--				bg_color = wez_theme.background,
+--				fg_color = wez_theme.foreground,
+--			},
+--			inactive_tab_hover = {
+--				bg_color = wezterm.color.parse(wez_theme.background):lighten(0.1),
+--				fg_color = wezterm.color.parse(wez_theme.foreground):lighten(0.2),
+--			},
+--			new_tab = {
+--				bg_color = wez_theme.background,
+--				fg_color = wez_theme.foreground,
+--			},
+--			new_tab_hover = {
+--				bg_color = wez_theme.brights[3],
+--				fg_color = wez_theme.background,
+--			},
+--		},
+--	}
+--end
 
-tabTheme.extract_tab_bar_colors_from_theme = function(theme_name)
-	local wez_theme = wezterm.color.get_builtin_schemes()[theme_name]
-	return {
-		window_frame_colors = {
-			active_titlebar_bg = wez_theme.background,
-			inactive_titlebar_bg = wezterm.color.parse(wez_theme.background):darken(0.8),
-		},
-		tab_bar_colors = {
-			inactive_tab_edge = wezterm.color.parse(wez_theme.background):darken(0.8),
-			active_tab = {
-				bg_color = wez_theme.brights[3],
-				fg_color = wez_theme.background,
-			},
-			inactive_tab = {
-				bg_color = wez_theme.background,
-				fg_color = wez_theme.foreground,
-			},
-			inactive_tab_hover = {
-				bg_color = wezterm.color.parse(wez_theme.background):lighten(0.1),
-				fg_color = wezterm.color.parse(wez_theme.foreground):lighten(0.2),
-			},
-			new_tab = {
-				bg_color = wez_theme.background,
-				fg_color = wez_theme.foreground,
-			},
-			new_tab_hover = {
-				bg_color = wez_theme.brights[3],
-				fg_color = wez_theme.background,
-			},
-		},
-	}
-end
-
+--local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 --tabline.setup({
 --	options = {
 --		icons_enabled = true,
@@ -81,4 +82,5 @@ end
 --	},
 --	extensions = {},
 --})
-return tabTheme
+
+--return tabTheme
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require("wezterm")
 local keybind = require("keybind")
-local tabTheme = require("tabTheme")
-local tab_bar_theme = tabTheme.extract_tab_bar_colors_from_theme("Catppuccin Mocha")
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+--local tabTheme = require("tabTheme")
+--local tab_bar_theme = tabTheme.extract_tab_bar_colors_from_theme("Catppuccin Mocha")
 local config = {
 	font_size = 12,
 	color_scheme = "Catppuccin Mocha",
@@ -12,13 +13,13 @@ local config = {
 	show_new_tab_button_in_tab_bar = false,
 	window_background_opacity = 0.7,
 	window_decorations = "RESIZE",
-	window_frame = tabTheme.merge_tables({
-		font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold", stretch = "Normal", style = "Normal" }),
-	}, tab_bar_theme.window_frame_colors, { font_size = 10 }),
+	--window_frame = tabTheme.merge_tables({
+	--	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold", stretch = "Normal", style = "Normal" }),
+	--}, tab_bar_theme.window_frame_colors, { font_size = 10 }),
 
-	colors = {
-		tab_bar = tab_bar_theme.tab_bar_colors,
-	},
+	--colors = {
+	--	tab_bar = tab_bar_theme.tab_bar_colors,
+	--},
 
 	adjust_window_size_when_changing_font_size = false,
 	text_background_opacity = 1,
@@ -32,6 +33,43 @@ local config = {
 		top = 0,
 		bottom = 0,
 	},
+	tabline.setup({
+		options = {
+			icons_enabled = true,
+			theme = "Catppuccin Mocha",
+			tabs_enabled = true,
+			theme_overrides = {},
+			section_separators = {
+				left = wezterm.nerdfonts.pl_left_hard_divider,
+				right = wezterm.nerdfonts.pl_right_hard_divider,
+			},
+			component_separators = {
+				left = wezterm.nerdfonts.pl_left_soft_divider,
+				right = wezterm.nerdfonts.pl_right_soft_divider,
+			},
+			tab_separators = {
+				left = wezterm.nerdfonts.pl_left_hard_divider,
+				right = wezterm.nerdfonts.pl_right_hard_divider,
+			},
+		},
+		sections = {
+			tabline_a = { "mode" },
+			tabline_b = { "workspace" },
+			tabline_c = { " " },
+			tab_active = {
+				"index",
+				{ "parent", padding = 0 },
+				"/",
+				{ "cwd", padding = { left = 0, right = 1 } },
+				{ "zoomed", padding = 0 },
+			},
+			tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
+			tabline_x = { "ram", "cpu" },
+			tabline_y = { "datetime", "battery" },
+			tabline_z = { "hostname" },
+		},
+		extensions = {},
+	}),
 }
-
+tabline.apply_to_config(config)
 return config
